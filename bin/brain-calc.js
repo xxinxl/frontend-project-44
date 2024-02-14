ee04a359ceecba01d9030d5714f1e8a8e775cf83
Natalia <natalia.lyamina1234@gmail.com>
2024-02-14 14:24:54 +0300

    step 6

---
+++ bin/brain-calc.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+import gameBody from '../src/gameBody.js';
+
+const description = 'What is the result of the expression?';
+
+const task = () => {
+    const operand1 = Math.round(Math.random() * 10);
+    const operand2 = Math.round(Math.random() * 10);
+
+    const operator = ['+', '*', '-'];
+    const indexOPer = Math.floor(Math.random() * 2);
+
+    let answer = '';
+
+    switch(indexOPer) {
+        case 0:
+            answer = `${operand1 + operand2}`;
+            break;
+        case 1:
+            answer = `${operand1 * operand2}`;
+            break;
+        default:
+             answer = `${operand1 - operand2}`;
+            break;
+    }
+
+    return [`${operand1} ${operator[indexOPer]} ${operand2}`, answer];
+};
+
+gameBody(description, task);
