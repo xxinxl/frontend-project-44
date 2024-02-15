b58c2f9fa5a2f04ae15a0dc67dcb1760f556f0c1
Natalia <natalia.lyamina1234@gmail.com>
2024-02-15 10:05:34 +0300

    step7

---
+++ bin/brain-gcd.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import gameBody from '../src/gameBody.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const task = () => {
+    const num1 = Math.round(Math.random() * 50);
+    const num2 = Math.round(Math.random() * 50);
+
+    let a = num1;
+    let b = num2;
+    while ( a !== 0 && b !== 0) {
+        if (a >= b) {
+            a -= b;
+        } else {
+            b -= a;
+        }
+    }
+    const answer = `${a + b}`;
+
+    return [`${num1} ${num2}`, answer];
+};
+
+gameBody(description, task);
