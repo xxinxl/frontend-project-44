6a38f1f67adba126b6c601ce420147d60e5a2c57
Natalia <natalia.lyamina1234@gmail.com>
2024-02-15 14:10:49 +0300

    fix progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -4,19 +4,19 @@ import gameBody from '../src/gameBody.js';
 const description = 'What number is missing in the progression?';
 
 const task = () => {
-    const arrayLength = Math.floor(Math.random() * 5) + 5;
-    const progressDelta = Math.round(Math.random() * 10);
-    const firstNumber = Math.round(Math.random() * 100);
-    const progression = [];
-    progression.push(firstNumber);
-    for (let i = 1; i < arrayLength; i += 1) {
-        progression.push(progression[i - 1] + progressDelta);
-    }
-    const emptyIndex = Math.floor(Math.random() * arrayLength) + 1;
-    const answer = `${progression[emptyIndex]}`;
-    progression[emptyIndex] = '..';
+  const arrayLength = Math.floor(Math.random() * 5) + 5;
+  const progressDelta = Math.round(Math.random() * 10);
+  const firstNumber = Math.round(Math.random() * 100);
+  const progression = [];
+  progression.push(firstNumber);
+  for (let i = 1; i < arrayLength; i += 1) {
+    progression.push(progression[i - 1] + progressDelta);
+  }
+  const emptyIndex = Math.floor(Math.random() * arrayLength);
+  const answer = `${progression[emptyIndex]}`;
+  progression[emptyIndex] = '..';
 
-    return [progression.join(' '), answer]; 
+  return [progression.join(' '), answer];
 };
 
 gameBody(description, task);
