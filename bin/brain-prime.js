f41fc04d359e82e753a005cbe0ac7ed8e7f66ff9
Natalia <natalia.lyamina1234@gmail.com>
2024-02-15 14:13:40 +0300

    fix problems

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -4,14 +4,14 @@ import gameBody from '../src/gameBody.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const task = () => {
-    const num = Math.round(Math.random() * 50);
+  const num = Math.round(Math.random() * 50);
 
-    for (let i = 2; i <= Math.sqrt(num); i += 1) {
-        if (num % i === 0) {
-         return [num, 'no'];
-        }
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return [num, 'no'];
     }
-    return [num, 'yes'];
+  }
+  return [num, 'yes'];
 };
 
 gameBody(description, task);
